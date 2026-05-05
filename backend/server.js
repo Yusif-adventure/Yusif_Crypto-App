4da4b3a9aff403e115977ecf22549046520d91fe
Yusif-adventure <yibrahim021@st.ug.edu.gh>
2026-05-05 00:24:03 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(cors({ 
   origin: process.env.NODE_ENV === "production" 
-    ? "https://netlify.app" 
+    ? "https://yusifibrahim-crypto-app.netlify.app" 
     : "http://localhost:5173", 
   credentials: true 
 }));
